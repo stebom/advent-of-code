babce051ad928c59d2457d3182aa93e85ac6a0e4
Stefan Páll Boman <stefan.p.boman@gmail.com>
2023-11-24 18:53:55 +0100

    Add 2017 day 10 part 2

--- csharp-aoc/Aoc2017/Day10.cs
+++ csharp-aoc/Aoc2017/Day10.cs
@@ -1,8 +1,31 @@
 ﻿namespace Aoc2017;
 static class Day10 {
     public static void Run() {
-        var numbers = File.ReadAllLines(@"input_day_10.txt").First().Split(',').Select(int.Parse).ToArray();
-        Process(Enumerable.Range(0, 256).ToArray(), numbers);
+        var input = File.ReadAllLines(@"input_day_10.txt").First();
+
+        {
+            // Part One
+            var lengths = input.Split(',').Select(int.Parse).ToArray();
+            var list = Enumerable.Range(0, 256).ToArray();
+            Process(list, lengths);
+            Console.WriteLine(list.Take(2).Aggregate((a, b) => a * b));
+        }
+
+        {
+            // Part Two
+            var sparse = Enumerable.Range(0, 256).ToArray();
+            var lengths = input.ToArray().Select(c => (int)c).Concat(new[] { 17, 31, 73, 47, 23 }).ToArray();
+
+            var pos = 0;
+            var skip = 0;
+            for (int i = 0; i < 64; i++) {
+                (pos, skip) = Process(sparse, lengths, pos, skip);
+            }
+
+            var dense = sparse.Chunk(16).Select(chunk => chunk.Aggregate((a, b) => a ^ b));
+            var hash = dense.Select(v => v.ToString("X2").ToLower()).ToArray();
+            Console.WriteLine(string.Join("", hash));
+        }
     }
 
     static IEnumerable<int> TakeWrap(this int[] list, int pos, int num) {
@@ -12,15 +35,9 @@ static class Day10 {
         }
     }
 
-    static void Process(int[] list, int[] lengths) {
-
-        int pos = 0;
-        int skip = 0;
+    static (int pos, int skip) Process(int[] list, int[] lengths, int pos = 0, int skip = 0) {
 
         foreach (var length in lengths) {
-
-            //Console.WriteLine($"pass {skip} pos: {pos} length: {length} list: {string.Join(',', list)}");
-
             // - Reverse the order of that length of elements in the list,
             //   starting with the element at the current position.
             // - Move the current position forward by that length plus the skip size.
@@ -40,8 +57,6 @@ static class Day10 {
             skip++;
         }
 
-        Console.WriteLine($"pass {skip} pos: {pos} list: {string.Join(',', list)}");
-
-        Console.WriteLine(list.Take(2).Aggregate((a, b) => a * b));
+        return (pos, skip);
     }
 }
