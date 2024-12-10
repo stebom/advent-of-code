25e44aed906d6cdc0eab68d13385cc0603f3c039
Stefan Boman <Stefan.Boman@elekta.com>
2024-12-10 06:38:32 +0100

    Optimize running time by parsing ints

--- csharp-aoc/Aoc2024/Day10.cs
+++ csharp-aoc/Aoc2024/Day10.cs
@@ -1,27 +1,23 @@
-﻿
-using System.Data;
-using System.IO;
-using System.Threading.Tasks.Sources;
-using static System.Net.Mime.MediaTypeNames;
-
-namespace Aoc2024;
+﻿namespace Aoc2024;
 
 public static class Day10
 {
+    private static readonly Cell[] Directions = [new(-1, 0), new(0, 1), new(1, 0), new(0, -1)];
+
     record struct Cell(int R, int C);
 
     public static void Solve()
     {
-        var grid = File.ReadAllLines(@"input/day10_input.txt").Select(l => l.ToCharArray()).ToArray();
+        var grid = File.ReadAllLines(@"input/day10_input.txt").Select(l => l.ToCharArray().Select(c => c - '0').ToArray()).ToArray();
 
-        var part1 = 0;
-        var part2 = 0;
+        int part1 = 0;
+        int part2 = 0;
 
         for (var r = 0; r < grid.Length; r++)
         {
             for (var c = 0; c < grid[r].Length; c++)
             {
-                if (grid[r][c] == '0')
+                if (grid[r][c] == 0)
                 {
                     var (score, rating) = Search(grid, new(r, c));
                     part1 += score;
@@ -33,44 +29,40 @@ public static class Day10
         Console.WriteLine(part1);
         Console.WriteLine(part2);
     }
-    private static readonly Cell[] Directions = [new(-1,0), new(0,1), new(1,0), new(0,-1)];
 
-    private static (int,int) Search(char[][] grid, Cell start)
+    private static (int,int) Search(int[][] grid, Cell start)
     {
+        var goals = new List<Cell>();
+
         var queue = new Queue<Cell>();
         queue.Enqueue(start);
-
-        Console.WriteLine($"Starting from {start}");
-
-        var goals = new HashSet<Cell>();
-        var score = 0;
 
         while (queue.Count > 0)
         {
             var cell = queue.Dequeue();
-            var height = grid[cell.R][cell.C] - '0';
+            var height = grid[cell.R][cell.C];
+
             if (height == 9)
             {
                 goals.Add(cell);
-                score++;
                 continue;
             }
 
             foreach (var direction in Directions)
             {
-                try
+                var next = new Cell(cell.R + direction.R, cell.C + direction.C);
+                if (next.R < 0 || next.R == grid.Length || next.C < 0 || next.C == grid[0].Length)
                 {
-                    var next = new Cell(cell.R + direction.R, cell.C + direction.C);
-                    var nextHeight = grid[cell.R + direction.R][cell.C + direction.C] - '0';
-                    if (nextHeight == height + 1)
-                    {
-                        queue.Enqueue(next);
-                    }
+                    continue;
                 }
-                catch { }
+
+                if (grid[next.R][next.C] == height + 1)
+                {
+                    queue.Enqueue(next);
+                }
             }
         }
 
-        return (goals.Count, score);
+        return (goals.Distinct().Count(), goals.Count);
     }
 }
