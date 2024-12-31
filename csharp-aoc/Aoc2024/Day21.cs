8e8138d4fcd244a530797a530ea57167297a8a0c
Stefan Boman <Stefan.Boman@elekta.com>
2024-12-31 09:23:20 +0100

    Day 21 part 1. Now memoization.

--- csharp-aoc/Aoc2024/Day21.cs
+++ csharp-aoc/Aoc2024/Day21.cs
@@ -2,229 +2,137 @@
 
 public static class Day21
 {
-    record struct Pair(char From, char To);
+    static readonly (int R, int C) Up       = (-1,0);
+    static readonly (int R, int C) Right    = (0, 1);
+    static readonly (int R, int C) Down     = (1, 0);
+    static readonly (int R, int C) Left     = (0, -1);
 
-    static readonly Dictionary<Pair, string[]> NumericalKeypad = new()
+    static readonly Dictionary<char, (int R, int C)> NumericKeypad = new()
     {
-        { new Pair('A','0'), ["<" ]},
-        { new Pair('A','1'), ["^<<" ]},
-        { new Pair('A','2'), ["<^" ]},
-        { new Pair('A','3'), ["^" ]},
-        { new Pair('A','4'), ["^^<<" ]},
-        { new Pair('A','5'), ["<^^" ]},
-        { new Pair('A','6'), ["^^" ]},
-        { new Pair('A','7'), ["^^^<<" ]},
-        { new Pair('A','8'), ["<^^^" ]},
-        { new Pair('A','9'), ["^^^" ]},
-
-        { new Pair('0','A'), [">"] },
-        { new Pair('0','1'), ["^<"] },
-        { new Pair('0','2'), ["^"]},
-        { new Pair('0','3'), [">^"] },
-        { new Pair('0','4'), ["^^<"] },
-        { new Pair('0','5'), ["^^"]},
-        { new Pair('0','6'), [">^^"] },
-        { new Pair('0','7'), ["^^^<"] },
-        { new Pair('0','8'), ["^^^"] },
-        { new Pair('0','9'), [">^^^"] },
-                             
-        { new Pair('1','A'), [">>v" ]},
-        { new Pair('1','0'), [">v"]},
-        { new Pair('1','2'), [">"] },
-        { new Pair('1','3'), [">>"] },
-        { new Pair('1','4'), ["^" ]},
-        { new Pair('1','5'), ["^>" ]},
-        { new Pair('1','6'), ["^>>"] },
-        { new Pair('1','7'), ["^^"] },
-        { new Pair('1','8'), ["^^>"]},
-        { new Pair('1','9'), ["^^>>"] },
-        { new Pair('2','A'), ["v>"] },
-        { new Pair('2','0'), ["v" ]},
-        { new Pair('2','1'), ["<"] },
-        { new Pair('2','3'), [">" ]},
-        { new Pair('2','4'), ["<^" ]},
-        { new Pair('2','5'), ["^" ]},
-        { new Pair('2','6'), ["^>" ]},
-        { new Pair('2','7'), ["<^^]"] },
-        { new Pair('2','8'), ["^^"]},
-        { new Pair('2','9'), ["^^>"]},
-                             
-        { new Pair('3','A'), ["v" ]},
-        { new Pair('3','0'), ["<v" ]},
-        { new Pair('3','1'), ["<<"] },
-        { new Pair('3','2'), ["<"] },
-        { new Pair('3','4'), ["<<^"] },
-        { new Pair('3','5'), ["<^"] },
-        { new Pair('3','6'), ["^"] },
-        { new Pair('3','7'), ["<<^^"] },
-        { new Pair('3','8'), ["<^^"] },
-        { new Pair('3','9'), ["^^"] },
-                             
-        { new Pair('4','A'), ["vv>>"] },
-        { new Pair('4','0'), ["vv>]"] },
-        { new Pair('4','1'), ["v" ] },
-        { new Pair('4','2'), ["v>]"] },
-        { new Pair('4','3'), ["v>>"] },
-        { new Pair('4','5'), [">"] },
-        { new Pair('4','6'), [">>"] },
-        { new Pair('4','7'), ["^"] },
-        { new Pair('4','8'), ["^>"] },
-        { new Pair('4','9'), ["^>>"] },
-                             
-        { new Pair('5','A'), ["vv>"]},
-        { new Pair('5','0'), ["vv" ]},
-        { new Pair('5','1'), ["<v" ]},
-        { new Pair('5','2'), ["v" ]},
-        { new Pair('5','3'), ["v>"] },
-        { new Pair('5','4'), ["<"] },
-        { new Pair('5','6'), [">"] },
-        { new Pair('5','7'), ["<^"] },
-        { new Pair('5','8'), ["^"] },
-        { new Pair('5','9'), ["^>"] },
-                             
-        { new Pair('6','A'), ["vv" ]},
-        { new Pair('6','0'), ["<vv" ]},
-        { new Pair('6','1'), ["<<v" ]},
-        { new Pair('6','2'), ["<v" ]},
-        { new Pair('6','3'), ["v" ]},
-        { new Pair('6','4'), ["<<"] },
-        { new Pair('6','5'), ["<"] },
-        { new Pair('6','7'), ["<<^"] },
-        { new Pair('6','8'), ["<^"] },
-        { new Pair('6','9'), ["^"] },
-                             
-        { new Pair('7','A'), [">>vvv" ]},
-        { new Pair('7','0'), [">vvv" ]},
-        { new Pair('7','1'), ["vv" ]},
-        { new Pair('7','2'), ["vv>"] },
-        { new Pair('7','3'), ["vv>>" ]},
-        { new Pair('7','4'), ["v" ]},
-        { new Pair('7','5'), ["v>]" ]},
-        { new Pair('7','6'), ["v>>" ]},
-        { new Pair('7','8'), [">"] },
-        { new Pair('7','9'), [">>"] },
-                             
-        { new Pair('8','A'), ["vvv>"] },
-        { new Pair('8','0'), ["vvv"]},
-        { new Pair('8','1'), ["<vv"]},
-        { new Pair('8','2'), ["vv"]},
-        { new Pair('8','3'), ["vv>"] },
-        { new Pair('8','4'), ["<v"]},
-        { new Pair('8','5'), ["v"]},
-        { new Pair('8','6'), ["v>"] },
-        { new Pair('8','7'), ["<"] },
-        { new Pair('8','9'), [">"] },
-                             
-        { new Pair('9','A'), ["vvv" ]},
-        { new Pair('9','0'), ["<vvv" ]},
-        { new Pair('9','1'), ["<<vv" ]},
-        { new Pair('9','2'), ["<vv" ]},
-        { new Pair('9','3'), ["vv" ]},
-        { new Pair('9','4'), ["<<v" ]},
-        { new Pair('9','5'), ["<v" ]},
-        { new Pair('9','6'), ["v" ]},
-        { new Pair('9','7'), ["<<"] },
-        { new Pair('9','8'), ["<"] },
+        { '7', (0,0) }, { '8', (0,1) }, { '9', (0,2) },
+        { '4', (1,0) }, { '5', (1,1) }, { '6', (1,2) },
+        { '1', (2,0) }, { '2', (2,1) }, { '3', (2,2) },
+                        { '0', (3,1) }, { 'A', (3,2) },
     };
 
-    static readonly Dictionary<Pair, string> DirectionalKeypad = new()
+    static readonly Dictionary<char, (int R, int C)> DirectionalKeypad = new()
     {
-        { new Pair('A','A'), "" },
-        { new Pair('A','^'), "<" },
-        { new Pair('A','<'), "<v<" },
-        { new Pair('A','v'), "<v" },
-        { new Pair('A','>'), "v" },
-
-        { new Pair('^','A'), ">" },
-        { new Pair('^','<'), "v<" },
-        { new Pair('^','^'), "" },
-        { new Pair('^','v'), "v" },
-        { new Pair('^','>'), "v>" },
-
-        { new Pair('v','A'), "^>" },
-        { new Pair('v','<'), "<" },
-        { new Pair('v','^'), "^" },
-        { new Pair('v','v'), "" },
-        { new Pair('v','>'), ">" },
-
-        { new Pair('<','A'), ">>^" },
-        { new Pair('<', '<'), "" },
-        { new Pair('<','v'), ">" },
-        { new Pair('<','^'), "^>" },
-        { new Pair('<','>'), ">>" },
-
-        { new Pair('>','A'), "^" },
-        { new Pair('>','v'), ">" },
-        { new Pair('>','^'), "<^" },
-        { new Pair('>','<'), "<<" },
-        { new Pair('>','>'), "" },
+                        { '^', (0,1) }, { 'A', (0,2) },
+        { '<', (1,0) }, { 'v', (1,1) }, { '>', (1,2) },
     };
-    
+
     public static void Solve()
     {
-        //string[] inputs = ["029A", "980A", "179A", "456A", "379A"];
         string[] inputs = ["208A", "586A", "341A", "463A", "593A"];
+        //string[] inputs = ["029A", "980A", "179A", "456A", "379A"];
 
         long sum = 0;
+
         foreach (var input in inputs)
         {
-            var expanded = input.Expand().ToList();
-            var complexity = long.Parse(input[0..(input.Length - 1)]);
+            List<List<char>> solutions = [];
+            foreach (var path in Search(NumericKeypad, new Queue<char>(input.ToCharArray())))
+            {
+                Search(solutions, path, 2);
+            }
 
-            sum += expanded.Count * complexity;
-
-            Console.WriteLine($"{input}: {string.Join("", expanded)} {expanded.Count} * {complexity}");
+            Console.WriteLine($"{solutions.Min(s => s.Count)} * {int.Parse(input[0..3])}");
+            sum += solutions.Min(s => s.Count) * int.Parse(input[0..3]);
         }
 
         Console.WriteLine($"Part 1: {sum}");
     }
 
-    static IEnumerable<char> Expand(this string input)
+    private static void Search(List<List<char>> solutions, List<char> path, int recurse)
     {
-        var pos = 'A';
-        var pos2 = 'A';
-        var pos3 = 'A';
-
-        List<List<char>> sequences = [[],[],[],[]];
-
-        foreach (var c in input)
+        if (recurse == 0)
         {
-            var p = NumericalKeypad[new Pair(pos, c)];
-            foreach (var pp in p)
-            { 
-                foreach (var c2 in pp.ZipButton())
-                {
-                    foreach (var c3 in DirectionalKeypad[new Pair(pos2, c2)].ZipButton())
-                    {
-                        foreach (var c4 in DirectionalKeypad[new Pair(pos3, c3)].ZipButton())
-                        {
-                            sequences[0].Add(c4);
-                            yield return c4;
-                        }
-
-                        sequences[1].Add(c3);
-                        pos3 = c3;
-                    }
-
-                    sequences[2].Add(c2);
-                    pos2 = c2;
-                }
-
-                sequences[3].Add(c);
-                pos = c;
-            }
+            solutions.Add(path);
+            return;
         }
 
-        Console.WriteLine();
-        foreach (var c in sequences)
+        foreach (var solution in Search(DirectionalKeypad, new Queue<char>(path)))
         {
-            Console.WriteLine(string.Join("", c));
+            Search(solutions, solution, recurse - 1);
         }
     }
 
-    static IEnumerable<char> ZipButton(this IEnumerable<char> sequence)
+    static List<List<char>> Search(Dictionary<char, (int R, int C)> map, Queue<char> initial)
     {
-        foreach (var c in sequence) yield return c;
-        yield return 'A';
+        List<List<char>> solutions = [];
+        var queue = new Queue<(List<char>, Queue<char> Q, char Current)>();
+        queue.Enqueue(([], initial, 'A'));
+
+        while (queue.TryDequeue(out var state))
+        {
+            var (path, q, current) = state;
+
+            if (q.Count == 0)
+            {
+                solutions.Add(path);
+                continue;
+            }
+
+            var next = q.Dequeue();
+            foreach (var possible in Search([.. map.Values], map[current], map[next]))
+            {
+                var presses = ConstructPresses([map[current], .. possible]);
+                queue.Enqueue(([.. path, .. presses], new Queue<char>(q), next));
+            }
+        }
+
+        return solutions;
+    }
+
+    static List<List<(int R, int C)>> Search(List<(int R, int C)> valid, (int R, int C) from, (int R, int C) to)
+    {
+        var paths = new List<List<(int R, int C)>>();
+        if (from == to) return [[]];
+
+        var directions = new List<(int R, int C)>();
+        if (from.R < to.R) directions.Add(Down);
+        else if (from.R > to.R) directions.Add(Up);
+
+        if (from.C < to.C) directions.Add(Right);
+        else if (from.C > to.C) directions.Add(Left);
+
+        var queue = new Queue<(List<(int R, int C)>, (int R, int C))>();
+        queue.Enqueue(([], from));
+
+        while (queue.TryDequeue(out var state))
+        {
+            var (path, current) = state;
+            if (current == to)
+            {
+                paths.Add(path);
+                continue;
+            }
+
+            foreach (var (r, c) in directions)
+            {
+                (int R, int C) next = (current.R + r, current.C + c);
+                if (!valid.Contains(next)) continue;
+
+                queue.Enqueue(([.. path, next], next));
+            }
+        }
+
+        return paths;
+    }
+
+    static List<char> ConstructPresses(List<(int R, int C)> path)
+    {
+        List<char> presses = [];
+        var current = path.First();
+        foreach (var next in path.Skip(1))
+        {
+            if (current.R < next.R) presses.Add('v');
+            else if (current.R > next.R) presses.Add('^');
+            else if (current.C < next.C) presses.Add('>');
+            else if (current.C > next.C) presses.Add('<');
+            current = next;
+        }
+        presses.Add('A');
+        return presses;
     }
 }
