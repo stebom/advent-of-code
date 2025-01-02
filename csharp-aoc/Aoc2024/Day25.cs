5dcecf3fa162fa8d258344c14450cff001cc251e
Stefan Boman <Stefan.Boman@elekta.com>
2025-01-02 11:58:06 +0100

    Day 25 (the only part)

---
+++ csharp-aoc/Aoc2024/Day25.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace Aoc2024;
+
+public static class Day25
+{
+    public static void Solve()
+    {
+        List<List<int>> locks = [];
+        var groups = File.ReadAllText("input/day25_input.txt").Split("\n\n");
+        foreach (var group in groups)
+        {
+
+            var rows = group.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+            Debug.Assert(7 == rows.Length);
+            Debug.Assert(rows.All(r => r.Length == 5));
+
+            var current = new List<int>();
+            for (var c = 0; c < 5; c++)
+            {
+                current.Add(Convert.ToInt16(new string(rows.Select(r => r[c] == '#' ? '1' : '0').ToArray()), 2));
+            }
+
+            locks.Add(current);
+        }
+
+        var fits = 0;
+        for (var i = 0; i < locks.Count; i++)
+        {
+            for (var y = i + 1; y < locks.Count; y++)
+            {
+                var test = Enumerable.Range(0, 5).All(c => (locks[i][c] & locks[y][c]) == 0);
+                fits += test ? 1 : 0;
+            }
+        }
+        Console.WriteLine(fits);
+    }
+}
